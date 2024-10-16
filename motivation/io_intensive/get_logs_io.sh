3cea5f792af1f66c262301c4c27d952542bc930a
Minjae Kang <austin9228@korea.ac.kr>
2024-10-16 11:41:59 +0900

    Add IO intensive function and benchmark
    - Added a new file `my_file.txt` for IO intensive function.
    - Created a new function `lambda_handler` in `io_intensive.py` to perform IO intensive operations.
    - The function reads the contents of `my_file.txt` 50,000 times.
    - Updated the `cpu_intensive_benchmark.py` file to perform CPU intensive calculations.
    - Updated the `mem_intensive.py` file to perform memory intensive operations on an image.
    - Added shell scripts `get_logs_io.sh`, `get_logs_mem.sh`, and `get_logs_cpu.sh` to retrieve and format logs for IO, memory, and CPU intensive benchmarks.
    - Added Python scripts `get_result_io.py`, `get_result_mem.py`, and `get_result_cpu.py` to invoke and benchmark the IO, memory, and CPU intensive functions respectively.

---
+++ motivation/io_intensive/get_logs_io.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+log_group_name="/aws/lambda/io_intensive_benchmark"
+output_file="io_intensive_log.csv"
+
+# 기존 output.csv 파일 삭제
+rm -f "$output_file"
+
+# CSV 헤더 작성
+echo "LogStreamName,Duration,BilledDuration,MemorySize,MaxMemoryUsed" >> "$output_file"
+
+# 로그 스트림 이름을 최근 발생 순서대로 가져오기 (오름차순 정렬)
+log_streams=$(aws logs describe-log-streams --log-group-name "$log_group_name" --order-by "LastEventTime" --descending --query 'logStreams[*].logStreamName' --output text)
+
+# 각 로그 스트림에서 REPORT 로그 이벤트만 가져오기
+for log_stream in $log_streams
+do
+    aws logs get-log-events --log-group-name "$log_group_name" --log-stream-name "$log_stream" --output text |
+    grep "REPORT" |  # REPORT 로그만 가져옴
+    awk '{
+        # Duration 추출
+        duration_index = index($0, "Duration: ");
+        billed_duration_index = index($0, "Billed Duration: ");
+        memory_size_index = index($0, "Memory Size: ");
+        max_memory_used_index = index($0, "Max Memory Used: ");
+        
+        duration = substr($0, duration_index + 10, 8);
+        billed_duration = substr($0, billed_duration_index + 17, 8);
+        memory_size = substr($0, memory_size_index + 12, 8);
+        max_memory_used = substr($0, max_memory_used_index + 17, 6);
+        
+        # 추출된 값을 CSV 형식으로 출력
+        printf "%s,%s,%s,%s,%s\n", "'$log_stream'", duration, billed_duration, memory_size, max_memory_used;
+    }' >> "$output_file"
+done
+
+echo "Filtered and formatted REPORT logs have been saved to $output_file"
